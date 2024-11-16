0d6ba0d322cc9d9132b4acf23e22a4acaab08761
Ingo <me@ingowolf.au>
2024-11-16 14:50:34 +1100

    Add landing sound

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,13 +6,17 @@ extends CharacterBody2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var landing = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		landing = true
+	if is_on_floor():
+		if landing:
+			$Land.play()
+			landing = false
 	# Handle Jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		$Jump.play()
