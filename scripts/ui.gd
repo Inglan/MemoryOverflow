b2cf2907ece63f87e6ec5819e917ced0bfc7eb6d
Ingo <me@ingowolf.au>
2024-11-17 18:54:15 +1100

    Add complteley nescasarry feature

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var sliderhovered = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +13,11 @@ func _process(delta):
 		$"Sound/navigation_backward-selection-minimal".play()
 		$Camera2D.position.y = $Title.position.y
 		$Camera2D.position.x = $Title.position.x
-
+	if Input.is_action_just_pressed("middlemouse") and sliderhovered:
+		$Title/VBoxContainer/HBoxContainer2/HSlider.value = 100
+		$Title/VBoxContainer/HBoxContainer2/Label.text = str(100000000000) + "%"
+		backgroundmusic.volume_db = linear_to_db(100000000000/100)
+		
 func back():
 	$"Sound/navigation_backward-selection-minimal".play()
 	$Camera2D.position.y = $Title.position.y
@@ -48,3 +53,11 @@ func tabchanged(tab):
 func volumechanged(value):
 	$Title/VBoxContainer/HBoxContainer2/Label.text = str(value) + "%"
 	backgroundmusic.volume_db = linear_to_db(value/100)
+
+
+func sliderhover():
+	sliderhovered = true
+
+
+func sliderunhover():
+	sliderhovered = false
