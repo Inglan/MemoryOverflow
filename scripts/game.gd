584a323b0de6a4d7bbe00bd0535dcd335ffafba2
Ingo <me@ingowolf.au>
2024-11-17 15:42:03 +1100

    Add fading effect on player position change

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var fading = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +11,13 @@ func _ready():
 func _process(delta):
 	if $Player.position[1]>1000:
 		$Player.velocity.y += $Player.gravity * delta * 5
+		
+		if not fading:
+			fading = true
+			$CanvasLayer/ColorRect/AnimationPlayer.play("fadeincolor")
+		
 	if $Player.position[1]>3000:
 		$Player.position = Vector2(-50, -1000)
 		$Player.velocity.y = 0
+		fading = false
+		$CanvasLayer/ColorRect/AnimationPlayer.play("fadeoutcolor")
