6b61af5db737eae77fa791e26c9fda7a04ea3ebd
Ingo <me@ingowolf.au>
2024-11-20 14:04:42 +1100

    add ram and migrate counter to global

---
+++ scripts/ramcounter.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var rambytes = float(10000)
+var ramusagebytes = float(0)
+var usagepercent = float(0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
