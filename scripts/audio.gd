c62bac0a2a7c017d79daddd298b74d64c795fd71
sebashtioon <sebastiansuciu607@gmail.com>
2024-12-19 21:51:19 +1100

    Clean up code
    remove unused functions and prefix parameters with _ to avoid errors

--- scripts/audio.gd
+++ scripts/audio.gd
@@ -1,12 +1,8 @@
 extends AudioStreamPlayer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$".".play()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if not $".".playing:
 		$".".play()
--- scripts/ramcounter.gd
+++ scripts/ramcounter.gd
@@ -4,11 +4,8 @@ var rambytes = float(10000)
 var ramusagebytes = float(0)
 var usagepercent = float(0)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
